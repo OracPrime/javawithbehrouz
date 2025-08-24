ea110879472b99239ca9a6316584dd9f3f65af64
karimi <behrouzkarimi1993@gmail.com>
2025-08-24 23:50:04 +0100

    ...

---
+++ project1/src/main/java/uk/co/cpsd/javaproject1/Traits.java
@@ -0,0 +1,11 @@
+package uk.co.cpsd.javaproject1;
+
+public class Traits {
+    String name;
+    double value;  // e.g., how strong this trait is, 0–10
+
+    public Traits(String name, double value) {
+        this.name = name;
+        this.value = value;
+    }
+}
